11def9023bea0300e25f9d7aa7daf165ce3370bb
Juan Carlos Banos Mafud <juancarlosbanosmafud@gmail.com>
2020-02-20 18:33:40 -0600

    Finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "json"
+
+Ingredient.destroy_all
+
+puts "creating ingredients"
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients = open(url).read
+ingredients_parsed = JSON.parse(ingredients)
+hash_of_ingredients = ingredients_parsed["drinks"]
+
+hash_of_ingredients.each { |hash| Ingredient.create(name: hash["strIngredient1"]) }
+
+puts "finished"
